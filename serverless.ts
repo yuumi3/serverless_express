af80c7835683a2a32c6807b5773deef2eb39f936
Yuumi Yoshida <yy@ey-office.com>
2021-03-07 18:36:48 +0900

    serverless + serverless-local

---
+++ serverless.ts
@@ -0,0 +1,32 @@
+import type { AWS } from '@serverless/typescript';
+
+import hello from '@functions/hello';
+
+const serverlessConfiguration: AWS = {
+  service: 'serverless3',
+  frameworkVersion: '2',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true,
+    },
+  },
+  plugins: ['serverless-webpack', 'serverless-offline'],
+  provider: {
+    name: 'aws',
+    region: 'ap-northeast-1',
+    runtime: 'nodejs12.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+    lambdaHashingVersion: '20201221',
+  },
+  // import the function via paths
+  functions: { hello },
+};
+
+module.exports = serverlessConfiguration;
